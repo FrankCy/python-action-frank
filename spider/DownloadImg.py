be50684efec2c32daaf3f352f79f65657a5b19fb
yang.chang <cy880708@163.com>
2021-03-17 15:09:33 +0800

    清空程序

--- spider/DownloadImg.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-response = urllib.request.urlopen('http://img16.3lian.com/gif2016/w1/3/d/61.jpg')
-image = response.read()
-
-with open('61.jpg','wb') as f:
-    f.write(image)
--- spider/WebsiteInfomation.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-# 访问网站
-response = urllib.request.urlopen('https://github.com')
-# 读取html
-html = response.read()
-# 输出结果
-print(html.decode('utf-8'))
